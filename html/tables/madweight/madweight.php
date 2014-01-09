815df9b8126cf5264356207526cc4b317e16b931
Christophe Delaere <christophe.delaere@cern.ch>
2014-01-09 21:28:52 +0100

    Correct definition of the isr correction options

--- html/tables/madweight/madweight.php
+++ html/tables/madweight/madweight.php
@@ -1,7 +1,7 @@
 <?php
 class tables_madweight {
     function valuelist__isrdefs() {
-      return array(0=>'Ignore effect', 1=>'by Pt(ISR)', 2=>'by Pt(ISR) + boost to CMS frame', 3=>'???');
+      return array(0=>'No Correction', 1=>'Correction by Pt(ISR)', 2=>'Correction by Pt(ISR) and boost to rest', 3=>'Unconstrained Pt(ISR)');
     }
     function valuelist__nwadefs() {
       return array(0=>'Disabled', 1=>'Enabled');
