cd972755af2936e232d762411ae0db47a833a5c9
Christophe Delaere <christophe.delaere@cern.ch>
2018-06-06 21:13:48 +0200

    add elog and analysis

--- scripts/add_result.py
+++ scripts/add_result.py
@@ -22,6 +22,12 @@ class MyOptionParser:
         self.parser.add_option("-d", "--description", action="store", type="string", 
                                default=None, dest="desc",
              help="description of the result")
+        self.parser.add_option("-e", "--elog", action="store", type="string",
+                               default=None, dest="elog",
+             help="elog with more details")
+        self.parser.add_option("-A", "--analysis", action="store", type="string",
+                               default=None, dest="ana",
+             help="analysis whose result belong to")
         self.parser.add_option("-a", "--author", action="store", type="string", 
                                default=None, dest="author",
              help="author of the result. If not specified, is taken from the path.")
@@ -63,6 +69,8 @@ def main():
     result.description = unicode(opts.desc)
     result.author = unicode(opts.author)
     result.creation_time = opts.datetime
+    result.elog = opts.elog
+    result.analysis_id = opts.ana
     # connect to the MySQL database using default credentials
     dbstore = DbStore()
     # unless the source is set, prompt the user and present a list to make a choice
