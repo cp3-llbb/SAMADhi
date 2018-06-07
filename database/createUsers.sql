8b897d186fc98674537abc7a0741f6008752dba0
Christophe Delaere <christophe.delaere@cern.ch>
2018-06-07 22:20:06 +0200

    drop the database directory

--- database/createUsers.sql
+++
@@ -1,4 +0,0 @@
-CREATE USER 'llbb'@'%' IDENTIFIED BY 'ijvIg]Em0geqME';
-CREATE USER 'llbb'@'localhost' IDENTIFIED BY 'ijvIg]Em0geqME';
-GRANT ALL PRIVILEGES ON `llbb`.* TO 'llbb'@'localhost';
-GRANT ALL PRIVILEGES ON `llbb`.* TO 'llbb'@'%';
--- database/v2_to_v3_upgrade.sql
+++
@@ -1,5 +0,0 @@
--- Upgrade SAMADhi from v2 to v3
--- Add a `processed_lumi` column to the sample table
-
--- Alter sample table
-ALTER TABLE sample ADD processed_lumi mediumtext;
